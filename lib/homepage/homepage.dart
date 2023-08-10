06e70fdd9513bbf21b41cd129e90042997ef24e7
Debjit Sarkar <68617086+Debjit0@users.noreply.github.com>
2023-08-10 19:15:22 +0530

    Update homepage.dart

--- lib/homepage/homepage.dart
+++ lib/homepage/homepage.dart
@@ -17,38 +17,103 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   String csv = "";
-  CollectionReference userCollection = FirebaseFirestore.instance.collection('Users');
+  CollectionReference userCollection =
+      FirebaseFirestore.instance.collection('Users');
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Hompage"),
       ),
       body: StreamBuilder(
-      stream: userCollection.snapshots(),
-      builder: (context,snapshot){
-        if (snapshot.hasError) {
-          return Text('Something went wrong');
-        }
+          stream: userCollection.snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Text('Something went wrong');
+            }
 
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Text("Loading");
-        }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Text("Loading");
+            }
 
-        return ListView.builder(
-          itemCount:snapshot.data!.docs.length,
-          itemBuilder: (context,index){
-            print(snapshot.data!.docs.length);
-            return ListTile(
-              title: Text(snapshot.data!.docs[index]['firstname'],style: TextStyle(color: Colors.white),),
-              subtitle: Text(snapshot.data!.docs[index]['isverified'].toString(),style: TextStyle(color: Colors.white)),
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                print(snapshot.data!.docs.length);
+                return GestureDetector(
+                  onLongPress: () {
+                    showDialog(
+                        context: context,
+                        builder: (ctx) => AlertDialog(
+                              title: Text("Are you sure?"),
+                              content: Text("Update verification status"),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.pop(context);
+                                    },
+                                    child: Text("Cancel")),
+                                TextButton(
+                                  onPressed: () {
+                                    if(snapshot.data!.docs[index]['isverified']==true){
+                                      changeToFalse(
+                                      id: snapshot.data!.docs[index].id,
+                                    );
+                                    Navigator.pop(context);
+                                    setState(() {});
+                                    }else{
+                                      changeToTrue(
+                                      id: snapshot.data!.docs[index].id,
+                                    );
+                                    Navigator.pop(context);
+                                    setState(() {});
+                                    }
+
+                                    
+                                  },
+                                  child: Text("ok"),
+                                )
+                              ],
+                            ));
+                  },
+                  child: ListTile(
+                    title: Text(
+                      snapshot.data!.docs[index]['firstname'],
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                        snapshot.data!.docs[index]['isverified'].toString(),
+                        style: TextStyle(color: Colors.white)),
+                  ),
+                );
+              },
             );
-          },
-        );
-      }),
+          }),
     );
   }
 
+  changeToTrue({@required String? id}) async {
+    print(id);
+    try {
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(id)
+          .update({"isverified": true});
+      print("trued");
+    } catch (e) {
+      print(e);
+    }
+  }
 
-
-
+  changeToFalse({@required String? id}) async {
+    print(id);
+    try {
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(id)
+          .update({"isverified": false});
+      print("falsed");
+    } catch (e) {
+      print(e);
+    }
+  }
 }
