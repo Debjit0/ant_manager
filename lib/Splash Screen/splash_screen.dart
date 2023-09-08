73a3ec7b4c6d93e1e04577a1dbbbac229284c2a2
Debjit Sarkar <68617086+Debjit0@users.noreply.github.com>
2023-09-09 02:54:06 +0530

    awd

--- lib/Splash Screen/splash_screen.dart
+++ lib/Splash Screen/splash_screen.dart
@@ -1,12 +1,12 @@
 
 import 'package:ant_manager/buttomnavbar/buttomNavBar.dart';
-import 'package:ant_manager/verified_influencer/verified_influencer.dart';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-import '../Check Verifiy/check_verify.dart';
+
 import '../Login Screen/login_screen.dart';
-import '../homepage/homepage.dart';
+
 import '../utils/routers.dart';
 
 
